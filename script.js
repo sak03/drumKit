827f48e022d6b9664ad0ac21e05cb7b7311c5d2e
sak03 <sartaj2394@gmail.com>
2022-02-13 12:27:45 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let audioClap = new Audio("clap.wav");
+let audioHat = new Audio("hat.wav");
+let audioKick = new Audio("kick.wav");
+let audioBoom = new Audio("boom.wav");
+let audioRide = new Audio("ride.wav");
+
+function playNow() {
+    audioClap.play();
+}
+playNow();
+
+function playHat() {
+    audioHat.play();
+}
+playHat();
+
+function playKick() {
+    audioKick.play();
+}
+playKick();
+
+function playBoom() {
+    audioBoom.play();
+}
+playBoom();
+
+function playRide() {
+    audioRide.play();
+}
+playRide();
